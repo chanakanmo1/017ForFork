e56fca1d3c083d7ba4a74acd95f3700ea93aa494
NJittakorn <166985281+NJittakorn@users.noreply.github.com>
2025-01-15 09:38:57 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,18 +6,7 @@
                 <h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font></center></h1>
                 <?php include "Date.php"; ?>
             </td>
-            <td width="43" rowspan="2">&nbsp;</td>
-        </tr>
-        <tr>
-            <td width="1007" bgcolor="#FFFFFF">
-                <img src="images/map.gif">
-            </td>
-            <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ</li>
-				<li><a href="register.php">สมัครสมาชิก</li>
+            ชิก</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
